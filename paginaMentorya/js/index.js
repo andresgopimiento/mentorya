2c1158aad997f7e81e480b9ec8307431f3f50beb
andresgopimiento <mrandrespimiento@gmail.com>
2024-02-05 10:23:55 -0500

    cambios traidos de sebas

---
+++ paginaMentorya/js/index.js
@@ -0,0 +1,14 @@
+/*-----BARRA DE NAVEGACIÓN------*/
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click",() => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click",() => {
+    nav.classList.remove("visible");
+
+})
